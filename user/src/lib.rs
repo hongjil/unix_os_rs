166cd78d9c69b97dc420259aef8623d74f48c110
Hongji Li <hongji.l@outlook.com>
2022-09-25 18:59:08 -0700

    Implement some dummies applications
    Milestone: rCore tutorial - Chapter 2.2
    
    Tested:
    n/a, will be done later.

---
+++ user/src/lib.rs
@@ -0,0 +1,41 @@
+#![feature(linkage)]
+#![feature(panic_info_message)]
+#![no_std]
+#[macro_use]
+
+pub mod console;
+mod lang_items;
+mod syscall;
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    // bss clearing should be done by OS :(
+    clear_bss();
+
+    // exit(main());
+    panic!("unreachable after sys_exit!");
+}
+
+// #[linkage = "weak"]
+// #[no_mangle]
+// fn main() -> i32 {
+//     panic!("Cannot find main!");
+// }
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+use syscall::*;
+
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+pub fn exit(exit_code: i32) -> isize {
+    sys_exit(exit_code)
+}
