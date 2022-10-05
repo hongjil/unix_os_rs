7df5a9c254d73dafa36448c730fa3e8915892c4e
Hongji Li <hongji.l@outlook.com>
2022-10-04 21:29:36 -0700

    Amend risc-v register docs and explain how stack tracing work.
    Milestone:
    n/a
    Tested:
    n/a

--- os/src/stack_trace.rs
+++ os/src/stack_trace.rs
@@ -1,5 +1,40 @@
 use core::{arch::asm, ptr};
-
+/*
+Stack
+                   .
+                   .
+      +->          .
+      |   +-----------------+   |
+      |   | return address  |   |
+      |   |   previous fp ------+
+      |   | saved registers |
+      |   | local variables |
+      |   |       ...       | <-+
+      |   +-----------------+   |
+      |   | return address  |   |
+      +------ previous fp   |   |
+          | saved registers |   |
+          | local variables |   |
+      +-> |       ...       |   |
+      |   +-----------------+   |
+      |   | return address  |   |
+      |   |   previous fp ------+
+      |   | saved registers |
+      |   | local variables |
+      |   |       ...       | <-+
+      |   +-----------------+   |
+      |   | return address  |   |
+      +------ previous fp   |   |
+          | saved registers |   |
+          | local variables |   |
+  $fp --> |       ...       |   |
+          +-----------------+   |
+          | return address  |   |
+          |   previous fp ------+
+          | saved registers |
+  $sp --> | local variables |
+          +-----------------+
+ */
 pub unsafe fn print_stack_trace() -> () {
     let mut fp: *const usize;
     asm!("mv {}, fp", out(reg) fp);
@@ -11,9 +46,14 @@ pub unsafe fn print_stack_trace() -> () {
 
         println!("0x{:016x}, fp = 0x{:016x}", saved_ra, saved_fp);
 
+        fp = saved_fp as *const usize;
+
+        // Looks like the frame stack looping doesn't work for exception.
+        // Instead, it points to 0x1 address instead of 0.
         // TODO: Investigate and fix the stack trace looping.
-        // fp = saved_fp as *const usize;
-        break;
+        if saved_fp == 1 {
+            break;
+        }
     }
     println!("== End stack trace ==");
 }
