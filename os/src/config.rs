63ff2e189b3984e22608485e04165392b002db96
Hongji Li <hongji.l@outlook.com>
2022-10-24 21:14:22 -0700

    [docs] Add the source of truth for CLOCK_FREQ in config
    Thanks @YdrMaster clarification: https://github.com/rcore-os/rCore-Tutorial-Book-v3/issues/97#issuecomment-1288291314
    
    Milestone:
    n/a
    
    Tested:
    n/a

--- os/src/config.rs
+++ os/src/config.rs
@@ -4,5 +4,9 @@ pub const APP_SIZE_LIMIT: usize = 0x20000;
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 
-pub const CLOCK_FREQ: usize = 12500000;
+// Execute following commands to get the CLOCK_FREQ for QEMU:
+//   qemu-system-riscv64 -machine virt,dumpdtb=dump.dtb
+//   dtc -o dump.dts dump.dtb
+// Then check `timebase-frequency` in cpus.
+pub const CLOCK_FREQ: usize = 10_000_000;
 pub const MICRO_PER_SEC: usize = 1_000_000;
